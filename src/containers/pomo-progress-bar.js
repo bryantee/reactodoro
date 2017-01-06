53462b718a6fa8caeed9c0b2dcb7d1c5dc809d0e
bryantee <bryantee@gmail.com>
2017-01-06 15:38:04 -0700

    Allow user to pause pomo and resume at anytime
    Resolves #2 but man is it getting janky...

--- src/containers/pomo-progress-bar.js
+++ src/containers/pomo-progress-bar.js
@@ -9,24 +9,46 @@ class PomoProgressBar extends React.Component {
       currentSeconds: 0,
       totalSeconds: (props.totalMinutes * 60),
       percentage: 0,
-      complete: false
+      complete: false,
+      isRunning: false,
+      isPaused: false
     }
 
     this.startPomo = this.startPomo.bind(this);
+    this.pausePomo = this.pausePomo.bind(this);
   }
 
   startPomo() {
-    setInterval( () => {
-      // check if pomo is complete
+
+    // set correct state each time startPomo is called
+    this.setState({
+      isRunning: true,
+      isPaused: false
+    });
+
+    // start setInterval for timer and return ID
+    // so that it can be cleared at anytime
+    let intervalId = setInterval( () => {
+      if (this.state.isPaused) {
+        clearInterval(intervalId);
+        return this.setState({ isRunning: false });
+      }
+      // check if pomo is complete, return if it is
       if (this.state.currentSeconds === this.state.totalSeconds + 1) {
         this.setState({ complete: true });
         return;
       }
+      // set the state for percentage during each loop through
       this.setState({
         currentSeconds: (this.state.currentSeconds + 1),
         percentage: ((this.state.currentSeconds / this.state.totalSeconds) * 100)
       });
     }, 1000);
+  }
+
+  pausePomo() {
+    console.log('Pause function called');
+    this.setState({ isPaused: true });
   }
 
   render () {
@@ -37,7 +59,7 @@ class PomoProgressBar extends React.Component {
           initialAnimation={true}
           textForPercentage={(percentage) => `${Math.ceil((((100 - percentage) / 100) * this.state.totalSeconds) / 60)}`}
         />
-        <button className="start-pomo-btn" onClick={this.startPomo}>Start Pomo</button>
+        <button className="start-pomo-btn" onClick={this.state.isRunning ? this.pausePomo : this.startPomo}>{this.state.isRunning ? 'Pause' : 'Start'}</button>
       </div>
     )
   }
