247cd6fe4f80605305de9fc006ffa432ebf29c0b
bryantee <bryantee@gmail.com>
2017-01-18 15:36:32 -0700

    Add a little documentation for mvp testing
    Slight cleanup to CSS styling

--- src/components/about.js
+++ src/components/about.js
@@ -4,7 +4,16 @@ const About = () => {
   return (
     <div>
       <h1>About Page</h1>
-      <p>This is a basic pomodoro app</p>
+      <p>This is a basic pomodoro app. To get started, click on the link in the upper left.</p>
+      <p>Try:</p>
+      <ul>
+        <li>Adding an activity to the list</li>
+        <li>Select an activity for your current pomodoro session</li>
+        <li>Click start to begin your session</li>
+        <li>When you're done, you'll be given the opportunity to take a break and catch up on some news</li>
+        <li>To return, click back on the pomodoro link and then click 'reset'</li>
+        <li>Rinse, repeat</li>
+      </ul>
     </div>
   )
 }
