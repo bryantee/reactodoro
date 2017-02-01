bad6b054de49a862cec178cde8842d8a8755a3df
bryantee <bryantee@gmail.com>
2017-02-01 15:48:36 -0700

    Make upper and lower bounds limited and reasonable for timer
    Resolves #22

--- src/components/change-time-form.js
+++ src/components/change-time-form.js
@@ -6,7 +6,7 @@ export default function ChangeTimeForm(props) {
   return (
     <form className="">
       <Slider
-        min={0}
+        min={15}
         max={30}
         defaultValue={25}
         value={props.value / 60}
